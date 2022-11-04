23904f33b1cf9b092d03b6b379c02a2cc7dad4fe
John Tranier <john.tranier@ticetime.com>
2022-11-04 08:32:20 +0100

    Composant <base-todo />

---
+++ src/components/dev/BaseTodo.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-banner dense rounded :class="color">
+    <template v-slot:avatar>
+      <q-icon name="schedule" color="primary" />
+    </template>
+    Le composant "<span class="text-weight-bold">{{ name }}</span
+    >" n'est pas encore implémenté.
+  </q-banner>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: String,
+  color: {
+    type: String,
+    default: 'white',
+  },
+});
+</script>
+<style scoped></style>
--- src/pages/MyAssignmentsPage.vue
+++ src/pages/MyAssignmentsPage.vue
@@ -1,5 +1,13 @@
-<template>TODO...</template>
+<template>
+  <base-todo name="AssignmentList" />
+</template>
 
 <script setup lang="ts"></script>
 
 <style scoped></style>
+<script>
+import BaseTodo from '../components/dev/BaseTodo.vue';
+export default {
+  components: { BaseTodo },
+};
+</script>
