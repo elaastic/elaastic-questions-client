1317517293a4db89968252c0b191599bb1c77441
John Tranier <john.tranier@ticetime.com>
2022-11-11 14:00:45 +0100

    Sequence state icons

--- src/components/assignment/SequenceSummary.vue
+++ src/components/assignment/SequenceSummary.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="row bordered q-pa-md items-center">
-    <div class="col-auto"><q-chip square size="sm">{{ num }}</q-chip></div>
+    <div class="col-auto">
+      <q-chip square size="sm">{{ num }}</q-chip>
+    </div>
     <div class="col text-weight-bold">{{ sequence.statement.title }}</div>
-    <div class="col-auto">{state}</div>
+    <div class="col-auto">
+      <q-icon :name="stateIcon" size="xl" color="grey" />
+    </div>
   </div>
-
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
+import { computed, PropType } from 'vue';
 import { Sequence } from 'src/models/assignment.interface';
 
-defineProps({
+const props = defineProps({
   sequence: {
     type: Object as PropType<Sequence>,
     required: true,
   },
   num: Number,
+});
+
+const stateIcon = computed((): string => {
+  const states = [
+    'edit',
+    'fact_check',
+    'pause',
+    'not_started',
+    'forum'
+  ];
+  return states[(props.num ?? 0) % states.length];
 });
 </script>
 
