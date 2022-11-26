b726a7e74d82885737b0f7309897b46a83cb20f8
John Tranier <john.tranier@ticetime.com>
2022-11-26 18:43:31 +0100

    Use FakerJS to generate random data

--- src/services/assignment-service.ts
+++ src/services/assignment-service.ts
@@ -1,14 +1,19 @@
 import { ServerAssignmentData } from 'src/models/assignment.interface';
 import { delay } from 'src/util/dev';
+import { faker } from '@faker-js/faker/locale/fr';
 
 async function getMyAssignments(): Promise<ServerAssignmentData[]> {
   await delay(2000); // simulate latency
 
-  return [
-    { lastUpdate: new Date().toString(), title: '1st assignment' },
-    { lastUpdate: new Date().toString(), title: '2nd assignment' },
-    { lastUpdate: new Date().toString(), title: '3rd assignment' },
-  ];
+  const nbResults = 3 + Math.round(10 * Math.random());
+  return [...Array(nbResults).keys()].map(() => {
+    return {
+      lastUpdate: faker.date
+        .between('2020-01-01T00:00:00.000Z', '2030-01-01T00:00:00.000Z')
+        .toString(),
+      title: faker.lorem.words(),
+    };
+  });
 }
 
 export const assignmentService = {
