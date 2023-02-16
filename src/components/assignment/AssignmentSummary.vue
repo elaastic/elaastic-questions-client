83a6b6511a5173325015e6830593283889b99e3e
John Tranier <john.tranier@ticetime.com>
2023-02-16 09:59:05 +0100

    Rewrite AssignmentSummary.vue

--- src/components/assignment/AssignmentSummary.vue
+++ src/components/assignment/AssignmentSummary.vue
@@ -1,13 +1,13 @@
 <template>
   <page-title icon="menu_book" :title="assignment.title" />
 
-  <q-banner class="bg-negative" v-if="assignment.nbSequence === 0">
+  <q-banner class="bg-negative" v-if="nbSequences === 0">
     There is no sequence in this assignment.
   </q-banner>
 
-  <div class="q-ma-sm sequences" v-if="sequences">
+  <div class="q-ma-sm sequences">
     <sequence-summary
-      v-for="(sequence, index) in sequences"
+      v-for="(sequence, index) in assignment.sequences"
       :key="sequence.id"
       :sequence="sequence"
       :num="index + 1"
@@ -21,8 +21,6 @@ import SequenceSummary from "components/assignment/SequenceSummary.vue";
 import { computed, PropType } from "vue";
 import PageTitle from "components/commons/PageTitle.vue";
 import { Assignment } from "src/models/assignment.interface";
-import { useAssignmentStore } from "stores/assignment-store";
-import { Sequence } from "src/models/sequence.interface";
 import { useRouter } from "vue-router";
 
 const props = defineProps({
@@ -32,23 +30,9 @@ const props = defineProps({
   },
 });
 
-const assignmentStore = useAssignmentStore();
+const nbSequences = computed(() => props.assignment.sequences.length);
+
 const router = useRouter();
-
-if (props.assignment.sequences.status === "NotLoadedYet") {
-  assignmentStore.loadSequences(props.assignment.id);
-}
-
-const sequences = computed((): Sequence[] | undefined => {
-  switch (props.assignment.sequences.status) {
-    case "NotLoadedYet":
-    case "Loading":
-      return undefined;
-
-    default:
-      return props.assignment.sequences.value;
-  }
-});
 
 function openSequence(sequenceIndex: number) {
   router.push({
