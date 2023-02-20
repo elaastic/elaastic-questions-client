b8bde29c39e90d12c1ec51ca1dd9883d678215b8
John Tranier <john.tranier@ticetime.com>
2023-02-20 11:35:31 +0100

    Fix Use Query Key Factory

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,8 +1,3 @@
-import { mergeQueryKeys } from "@lukemorales/query-key-factory";
-import { assignmentKeys } from "src/services/assignment.query";
-
-export const queries = mergeQueryKeys(assignmentKeys)
-
 export const defaultOptions = {
   staleTime: 1000 * 60 * 5, // 5 min
 }
